fcf830d15e9d164ac04b383b67b80beae4d49210
CZR000 <129563250+CZR000@users.noreply.github.com>
2025-10-21 10:35:23 -0500

    Update __manifest__.py

--- lf_pos_required_customer/__manifest__.py
+++ lf_pos_required_customer/__manifest__.py
@@ -23,7 +23,7 @@ before processing the order in the point of sale.
         ],
     },
     "images": [
-        "static/description/banner.png", 
+        "static/description/banner.jpg", 
         "static/description/icon.png"
     ],
     'module_type': 'official',
