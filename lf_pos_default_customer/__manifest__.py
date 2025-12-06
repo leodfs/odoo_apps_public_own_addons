de09ab1894423a90901f145e67bd09546f641fc4
leodfs <l30dfs@gmail.com>
2025-12-05 20:27:21 -0500

    [ADD] update addons

--- lf_pos_default_customer/__manifest__.py
+++ lf_pos_default_customer/__manifest__.py
@@ -4,6 +4,7 @@
     'author': 'Leo Daniel FS',
     'maintainer': 'Leo Daniel FS',
     'support': 'l30dfs@gmail.com',
+    'website': 'https://www.linkedin.com/in/leo-daniel-flores',
     'summary': 'Default customer for each point of sale',
     'description': """
 This module allows you to configure a default client for each point of sale.
